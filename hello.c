a2b034524b1cebbdc3fd141abdd19309bd764233
LeviLucena <LeviLucena@users.noreply.github.com>
2022-06-07 12:43:53 +0000

    automated commit by check50 [check50=True]

--- hello.c
+++ hello.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <cs50.h> //adicionar a biblioteca cs50
 
-int main (void) {
-    //get string é uma função específica da biblioteca do cs50!
-string nome = get_string("Qual é seu nome? \n");
+int main(void)
+{
 
-printf("olá,%s \n" ,nome);
+    //get string é uma função específica da biblioteca do cs50!
+    string nome = get_string("Qual é seu nome? \n");
+
+    printf("olá,%s \n", nome);
 }
