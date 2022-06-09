8b0354004c0a48805db4ab8dc92d262e7decf84c
LeviLucena <sr_lucen@yahoo.com.br>
2022-06-09 12:23:03 +0000

    commit

--- CS50/pset1/hello/hello.c
+++ CS50/pset1/hello/hello.c
@@ -1,9 +1,11 @@
-//Small program to hello people by their names
-#include <cs50.h>
 #include <stdio.h>
+#include<cs50.h>
 
 int main(void)
 {
-    string name = get_string("what is your name?\n");
-    printf("hello, %s\n", name);
+    //Gets the name from the user
+    string name = get_string("What's your name?\n");
+
+    //Prints out the salutation plus te name
+    printf("\nHello, %s!\n", name);
 }
