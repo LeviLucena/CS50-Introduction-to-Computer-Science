d67dd6a11c1bc10cc86fd98a4c96a83a23e7a9db
Levi Lucena <sr_lucen@yahoo.com.br>
2022-06-13 17:41:14 -0300

    commit

--- CS50/pset1/cash/cash.c
+++ CS50/pset1/cash/cash.c
@@ -1,40 +1,49 @@
-//Program based on Greedy Algorithm
-//Minimze the number of coins in change as much as possible
-#include <cs50.h>
 #include <stdio.h>
+#include <cs50.h>
 #include <math.h>
+
+int getChange(void);
+void printCoins(int);
 
 int main(void)
 {
-    int change = 0;
-    float owed;
-    //getting positive number
+    // Get change from user
+    int change = getChange();
+
+    // Print the number of coins required for change
+    printCoins(change);
+}
+
+int getChange(void)
+{
+    float change;
     do
     {
-        owed = get_float("Change owed: ");
+        change = get_float("Change owed: "); // cs50 library for accepting an i/p which is float(32-bit decimal number)
     }
-    while (owed < 0);
-    //converting the float or just my solution to deal with them
-    owed *= 100;
-    while (round(owed) >= 25)
-    {
-        owed -= 25;
-        change++;
-    }
-    while (round(owed) >= 10)
-    {
-        owed -= 10;
-        change++;
-    }
-    while (round(owed) >= 5)
-    {
-        owed -= 5;
-        change++;
-    }
-    while (round(owed) >= 1)
-    {
-        owed -= 1;
-        change++;
-    }
-    printf("%i\n", change);
+    while (change <= 0); // Reject any zeros or negative numbers
+    return round(100 * change);
+}
+
+void printCoins(int change)
+{
+    int cents25, cents10, cents5, cents1, totalCoins;
+
+    // Number of 25 cent coins required
+    cents25 = change / 25;
+    change -= (cents25 * 25);
+
+    // Number of 10 cent coins required
+    cents10 = change / 10;
+    change -= (cents10 * 10);
+
+    // Number of 5 cent coins required
+    cents5 = change / 5;
+    change -= (cents5 * 5);
+
+    // Number of 1 cent coins required
+    cents1 = change;
+
+    totalCoins = cents25 + cents10 + cents5 + cents1;
+    printf("%d\n", totalCoins);
 }
